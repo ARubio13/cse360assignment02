5c9695b568e21f531a6ee7aad0bbf093d4e5c8bf
Adan Rubio <70557678+ARubio13@users.noreply.github.com>
2020-10-05 22:25:47 -0700

    Create AddingMachine.java

---
+++ AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assignment02;
+
+/**
+* This is for Assignment 01. Version Control
+*
+* @author Adan Rubio
+*
+*/
+
+public class AddingMachine {
+  private int total;
+
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+
+/**
+* getTotal method should return the total value so far.
+*
+* @return 0
+*/
+
+  public int getTotal () {
+    return 0;
+  }
+
+/**
+* The add method should add value to total.
+*
+* @param value
+*/
+
+  public void add (int value) { 
+  }
+
+/**
+* The subtract method should subtract value from total.
+*
+* @param value
+*/
+
+  public void subtract (int value) {
+  }
+
+/**
+* The toString method should show the operations done so far.
+*
+* @return ""
+*/
+
+  public String toString () {
+    return "";
+  }
+
+/**
+* The clear method should reset the total
+*/
+
+  public void clear() {
+  }
+}
